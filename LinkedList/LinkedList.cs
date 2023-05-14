3181e17342707cd4a349f7d57f9811d04a98a2e4
Rahulalvk <rahulal432@gmail.com>
2023-05-13 23:18:07 -0700

    Linked List of 56, 30 and 70

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+
+    public class LinkedList
+    {
+        public Node Head;
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedList 
+{
+   
+            public class Program
+        {
+            public static void Main(string[] args)
+            {
+                LinkedList linkedList = new LinkedList();
+
+                linkedList.Add(56);
+                linkedList.Add(30);
+                linkedList.Add(70);
+
+                linkedList.Display();
+            }
+       }
+}
+    
